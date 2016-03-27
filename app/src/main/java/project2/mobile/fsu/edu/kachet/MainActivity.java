80711334974cfe33e6c54c8ff22e4ec0db3052e0
Tyler Hunnefeld <hunnefeldtyler@gmail.com>
2016-03-27 08:39:28 -0400

    Marker Addition & Location updates
    Changes:
    
    Added custom marker (the logo) pinned to user location
    
    Finished location
    
    Added helper function to resizes bitmaps
    
    Note: I used two methods and I think there is some degradation in
    quality on the marker. If that’s not just on my end, I’ll probably have
    to create a png that is the size we want and skip expensive
    calculations the app has to do

--- app/src/main/java/project2/mobile/fsu/edu/kachet/MainActivity.java
+++ app/src/main/java/project2/mobile/fsu/edu/kachet/MainActivity.java
@@ -2,6 +2,9 @@ package project2.mobile.fsu.edu.kachet;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
@@ -9,7 +12,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -18,13 +20,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MainActivity extends AppCompatActivity
-    implements OnMapReadyCallback,
+        implements
+        OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
@@ -33,6 +37,8 @@ public class MainActivity extends AppCompatActivity
     private GoogleApiClient mGoogleApiClient;
     private LatLng currLoc;
     private Marker locMarker;
+
+    private Marker myLocationMarker;
 
     //**************************************
     // Activity Lifecycle
@@ -73,19 +79,38 @@ public class MainActivity extends AppCompatActivity
                 != PackageManager.PERMISSION_GRANTED) {
             CoordinatorLayout mCoordLayout = (CoordinatorLayout) findViewById(R.id.coordLayout);
             Snackbar.make(mCoordLayout,
-                            "You do not have the proper permission to access current location.",
-                            Snackbar.LENGTH_INDEFINITE).show();
+                    "You do not have the proper permission to access current location.",
+                    Snackbar.LENGTH_INDEFINITE).show();
+
+
             return;
         }
         Location mLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
         if (mLocation != null) {
             currLoc = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
-            locMarker = gMap.addMarker(new MarkerOptions().position(currLoc).title("You!"));
+            locMarker = gMap.addMarker(new MarkerOptions()
+                    .position(currLoc)
+                    .title("You!")
+                    .icon(BitmapDescriptorFactory.fromBitmap(resizeMapIcons("splash", 150, 150))));
             gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currLoc, 5));
             gMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
         }
     }
+
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -113,20 +138,42 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
     //**************************************
     // Helper Functions
     //**************************************
 
-    private void addMarkerFromCoordinates (double Lat, double Long, final String markerTitle) {
-        LatLng newLocation = new LatLng(Lat, Long);
-        Marker newMarker = gMap.addMarker(new MarkerOptions().position(newLocation).title(markerTitle));
 
-        //If we want to keep track of the markers, should put them in an array or something
-        //TODO: I know Evan did this differently than I did for mine. I need to see why he did it the way he did and improve my code
-        //markerArray.add(newMarker);
+
+
+
+    private void addMarkerFromCoordinates(double Lat, double Long, final String markerTitle) {
+        LatLng newLocation = new LatLng(Lat, Long);
+        Marker newMarker = gMap.addMarker(new MarkerOptions()
+                .position(newLocation)
+                .title(markerTitle));
+
     }
 
+    public Bitmap resizeMapIcons(String iconName,int width, int height){
+        Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(), getResources().getIdentifier(iconName, "mipmap", getPackageName()));
+        //Bitmap resizedBitmap = Bitmap.createScaledBitmap(imageBitmap, width, height, false);
+        return getResizedBitmap(imageBitmap, height, width);
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth)
+    {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // create a matrix for the manipulation
+        Matrix matrix = new Matrix();
+        // resize the bit map
+        matrix.postScale(scaleWidth, scaleHeight);
+        // recreate the new Bitmap
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        return resizedBitmap;
+    }
 
 
 }
