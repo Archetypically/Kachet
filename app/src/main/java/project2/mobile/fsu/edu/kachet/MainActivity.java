ad34a6c2ddc7e72086cadffe72347012f310f2a4
Hoekshot <hoekshot13@gmail.com>
2016-03-29 00:42:26 -0400

    Added code to capture images Added onActivityResult, captureImage, getOutputMediaFileUri, and getOutputMediaFile. Use captureImage to get image and update currentMediaUri. Added WRITE_EXTERNAL_STORAGE permission.

--- app/src/main/java/project2/mobile/fsu/edu/kachet/MainActivity.java
+++ app/src/main/java/project2/mobile/fsu/edu/kachet/MainActivity.java
@@ -12,8 +12,11 @@ import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.location.LocationListener;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -47,8 +50,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -66,6 +72,13 @@ public class MainActivity extends AppCompatActivity
         ResultCallback {
 
     private final static String TAG = "MainActivity";
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    private static final String MEDIA_FOLDER_NAME = "Kachet";
+    private Uri currentMediaUri;
 
     private final static int fenceRadius = 50;
     private MapFragment mapFragment;
@@ -376,5 +389,112 @@ public class MainActivity extends AppCompatActivity
                         .fillColor(Color.parseColor("#66FCE4EC"))
                         .strokeColor(Color.parseColor("#99EC407A"))
         );
+    }
+
+    //**************************************
+    // Camera Functions
+    //**************************************
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // React to captured image
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG, "onActivityResult: Image saved to: " + currentMediaUri.getPath());
+
+                // TODO: Do something useful with saved image, located at currentMediaUri
+            }
+            else if (resultCode == RESULT_CANCELED) {
+                Log.i(TAG, "onActivityResult: User cancelled the image capture");
+            }
+            else {
+                // Image capture failed, advise user
+                // TODO: Figure out how best to advise user
+            }
+        }
+
+        // React to captured video
+        if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG, "onActivityResult: Video saved to: " + data.getData());
+
+                // TODO: Do something useful with saved video, located at currentMediaUri
+            }
+            else if (resultCode == RESULT_CANCELED) {
+                Log.i(TAG, "onActivityResult: User cancelled the video capture");
+            }
+            else {
+                // Video capture failed, advise user
+                // TODO: Figure out how best to advise user
+            }
+        }
+    }
+
+    /*
+     * Uses an image capture intent to obtain an image from the device's camera
+     */
+    void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Create a file to save the image
+        currentMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        if (currentMediaUri == null) {
+            Log.e(TAG, "CaptureImage: could not create file URI");
+            return;
+        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, currentMediaUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // TODO: Add ability to capture video
+
+    /*
+     * Creates a file Uri for saving an image or video
+     */
+    private static Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /*
+     * Creates a File for saving an image or video
+     */
+    private static File getOutputMediaFile(int type) {
+        // Check that external storage is mounted
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Log.e(TAG, "getOutputMediaFile: External storage is not mounted");
+            return null;
+        }
+
+        // Set the directory in which the file will be stored
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), MEDIA_FOLDER_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.e(TAG, "getOutputMediaFile: failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        }
+        else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "VID_" + timeStamp + ".mp4");
+        }
+        else {
+            return null;
+        }
+
+        return mediaFile;
     }
 }
